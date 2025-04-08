8112971a2b7fee9b7c7d20799bc7dc9d71548b1c
brandon-snider <brandon@snider.so>
2025-04-07 21:38:57 -0700

    Transformer Implementation

--- tests/adapters.py
+++ tests/adapters.py
@@ -4,7 +4,6 @@ import os
 from typing import IO, Any, BinaryIO
 from collections.abc import Iterable
 import cs336_basics
-import cs336_basics
 from jaxtyping import Float, Int
 
 import numpy.typing as npt
